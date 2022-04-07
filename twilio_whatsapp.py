657baf72dc2330a6f37c4ceb8d0ae4fbe259f3e9
Paari1263 <91940251+Paari1263@users.noreply.github.com>
2022-04-07 17:27:21 +0530

    Update twilio_whatsapp.py

--- twilio_whatsapp.py
+++ twilio_whatsapp.py
@@ -4,6 +4,8 @@ import os
 from twilio.rest import Client 
 from twilio.http.http_client import TwilioHttpClient
 
+# Error exception for invalid format of location
+
 while True:
     try:
         location = str(input("Enter your location : "))
@@ -11,6 +13,8 @@ while True:
     except ValueError:
         print("It is not a location! Please try again ...")
 print("Great, you successfully entered the location!")
+
+# OpenWeather API for requesting the longitude and latitude from a location
 
 ow_api_key = os.environ['OW_API_KEY']
 lon_lat_parameters = {
@@ -22,6 +26,8 @@ response = requests.get(url="http://api.openweathermap.org/geo/1.0/direct",param
 response.raise_for_status()
 latitude = str(response.json()[0]["lat"]).split(".")[0]
 longitude = str(response.json()[0]["lon"]).split(".")[0]
+
+# OpenWeather API for requesting the weather condition of the respective location
 
 parameters = {
     "lat":int(latitude),
@@ -36,6 +42,8 @@ weather_data = response.json()
 weather_slice = weather_data["hourly"][:12]
 will_rain = False
 
+# Checks the weather condition of that respective location by the weather status code
+
 for hour_data in weather_slice:
     condition_code = hour_data["weather"][0]["id"]
     if int(condition_code)<=700:
@@ -43,10 +51,15 @@ for hour_data in weather_slice:
 
 ph_no = str(input("Enter your phone number with your LAN extension : "))
 
+# Twilio credentials
+# This section is to be filled by the user by filling the environment variables
+
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 twilio_ph_no = os.environ['TWILIO_WHATSAPP_NO']
 your_ph_no = ph_no
+
+# This sends the alert message through the twilio whatsapp number to the user phone number
 
 if(will_rain):
     client = Client(account_sid, auth_token)
